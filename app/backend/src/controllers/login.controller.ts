cd9fc818c31869c298ad0e54759c24cdb06139a3
vitorbss12 <vitor_bss10@hotmail.com>
2022-09-29 11:22:55 -0300

    feat: Controller para login

---
+++ app/backend/src/controllers/login.controller.ts
@@ -0,0 +1,14 @@
+import { Response } from 'express';
+import RequestUser from '../interfaces/requestUser.interface';
+import IUser from '../interfaces/user.interface';
+import jsonwebtoken from '../middlewares/jwt.middleware';
+
+export default class LoginController {
+  static login = async (req: RequestUser, res: Response) => {
+    const { user } = req;
+
+    const token: string = await jsonwebtoken.generateTokenJWT(user as IUser);
+
+    return res.status(200).json({ token });
+  };
+}
