08eb466ada1db117beea3ed129b8708e2043376d
vitorbss12 <vitor_bss10@hotmail.com>
2022-09-29 12:13:38 -0300

    feat: Implementando método para validar token

--- app/backend/src/middlewares/jwt.middleware.ts
+++ app/backend/src/middlewares/jwt.middleware.ts
@@ -1,9 +1,16 @@
+import { Response, NextFunction } from 'express';
 import * as jwt from 'jsonwebtoken';
+import RequestUser from '../interfaces/requestUser.interface';
+// import LoginService from 'src/services/login.service';
+import HttpException from '../shared/http.exception';
 import IUser from '../interfaces/user.interface';
+import IDecoded from '../interfaces/decoded.interface';
 
 const secret: string = process.env.JWT_SECRET || 'jwt_secret';
 
 export default class jsonwebtoken {
+  static decoded: IDecoded;
+
   public static async generateTokenJWT(user: IUser): Promise<string> {
     const JwtConfig: jwt.SignOptions = {
       expiresIn: '1d',
@@ -13,4 +20,21 @@ export default class jsonwebtoken {
 
     return token;
   }
+
+  static isTokenValid = async (
+    req: RequestUser,
+    _res: Response,
+    next: NextFunction,
+  ) => {
+    const token = req.headers.authorization;
+
+    if (!token) {
+      throw new HttpException(401, 'Token not provided');
+    }
+
+    this.decoded = jwt.verify(token, secret) as IDecoded;
+
+    req.role = this.decoded.data.role;
+    return next();
+  };
 }
