1db76651c3b1b16a2ae0850023f46b2f4afe8a77
vitorbss12 <vitor_bss10@hotmail.com>
2022-09-30 13:10:56 -0300

    feat: Adicionando método findByPk na model Teams

--- app/backend/src/models/teams.models.ts
+++ app/backend/src/models/teams.models.ts
@@ -5,4 +5,8 @@ export default class TeamsModel {
   static async findAll(): Promise<ITeam[]> {
     return Teams.findAll();
   }
+
+  static async findByPk(id: number): Promise<ITeam | null> {
+    return Teams.findByPk(id);
+  }
 }
