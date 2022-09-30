2952dfcbc84e4d98dd8392d92e7768e774a50238
vitorbss12 <vitor_bss10@hotmail.com>
2022-09-30 10:29:33 -0300

    chore: Corrigindo eol para LF e comentando funções ainda não utilizadas

--- app/backend/src/middlewares/bcrypt.middleware.ts
+++ app/backend/src/middlewares/bcrypt.middleware.ts
@@ -3,9 +3,9 @@ import * as bcrypt from 'bcryptjs';
 export default class Bcrypt {
   public static salt = 10;
 
-  public static encrypt(password: string): string {
-    return bcrypt.hashSync(password, this.salt);
-  }
+  // public static encrypt(password: string): string {
+  //   return bcrypt.hashSync(password, this.salt);
+  // }
 
   public static compare(password: string, hash: string): boolean {
     return bcrypt.compareSync(password, hash);
--- app/backend/src/models/user.models.ts
+++ app/backend/src/models/user.models.ts
@@ -3,6 +3,8 @@ import IUser from '../interfaces/user.interface';
 
 export default class UserModel {
   static async findByEmail(email: string): Promise<IUser | null> {
-    return User.findOne({ where: { email } });
+    const user = await User.findOne({ where: { email } });
+    console.log(user);
+    return user;
   }
 }
