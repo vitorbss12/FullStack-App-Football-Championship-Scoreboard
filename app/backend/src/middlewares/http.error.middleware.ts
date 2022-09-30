e2c88dbc082f4abf0404f5af74a1cbbe2136b611
vitorbss12 <vitor_bss10@hotmail.com>
2022-09-30 19:13:31 -0300

    feat: Adicionando tratamento de erros para JWT

--- app/backend/src/middlewares/http.error.middleware.ts
+++ app/backend/src/middlewares/http.error.middleware.ts
@@ -3,6 +3,9 @@ import HttpException from '../shared/http.exception';
 
 const httpErrorMiddleware = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
   const { status, message } = err as HttpException;
+  if (message === 'invalid token' || message === 'jwt malformed') {
+    return res.status(401).send({ message: 'Token must be a valid token' });
+  }
   return res.status(status || 500).json({ message });
 };
 
