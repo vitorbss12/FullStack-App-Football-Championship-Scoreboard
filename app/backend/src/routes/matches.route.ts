4a98dade1a03b88a36b09ebd2d52b3670f8623a5
vitorbss12 <vitor_bss10@hotmail.com>
2022-09-30 18:54:40 -0300

    feat: Rota Post para partidas

--- app/backend/src/routes/matches.route.ts
+++ app/backend/src/routes/matches.route.ts
@@ -1,11 +1,21 @@
 import { Router } from 'express';
 import MatchesController from '../controllers/matches.controller';
+import createMatchValidation from '../middlewares/matchValidation.middleware';
+import jsonwebtoken from '../middlewares/jwt.middleware';
 
 const matchesRouter = Router();
 
 matchesRouter.get(
   '/',
   MatchesController.findAll,
+);
+matchesRouter.post(
+  '/',
+  jsonwebtoken.isTokenValid,
+  createMatchValidation.isCreateMatchBodyValid,
+  createMatchValidation.isTeamsEqual,
+  createMatchValidation.isTeamsValid,
+  MatchesController.create,
 );
 
 export default matchesRouter;
