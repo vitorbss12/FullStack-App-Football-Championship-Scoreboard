3c4699e58507e3983f4be265335dea83aac5e407
vitorbss12 <vitor_bss10@hotmail.com>
2022-09-30 10:30:05 -0300

    chore: Removendo console.log

--- app/backend/src/models/user.models.ts
+++ app/backend/src/models/user.models.ts
@@ -3,8 +3,6 @@ import IUser from '../interfaces/user.interface';
 
 export default class UserModel {
   static async findByEmail(email: string): Promise<IUser | null> {
-    const user = await User.findOne({ where: { email } });
-    console.log(user);
-    return user;
+    return User.findOne({ where: { email } });
   }
 }
