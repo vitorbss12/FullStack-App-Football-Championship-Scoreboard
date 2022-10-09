4c44e47f85331dba321cf84ac4ae683108788bd4
vitorbss12 <vitor_bss10@hotmail.com>
2022-10-09 20:27:12 -0300

    Controller e rota para Home Leaderboard

--- app/backend/src/controllers/leaderboard.controller.ts
+++ app/backend/src/controllers/leaderboard.controller.ts
@@ -2,6 +2,11 @@ import { Response, Request } from 'express';
 import LeaderboardsService from '../services/leaderboards.sevice';
 
 export default class LeaderboardsController {
+  static async homeTeamsGetAll(_req: Request, res: Response) {
+    const homeTeams = await LeaderboardsService.homeTeamsGetAll();
+    return res.status(200).json(homeTeams);
+  }
+
   static async findAll(_req: Request, res: Response) {
     const leaderboards = await LeaderboardsService.getTeamsInfo();
     res.status(200).json(leaderboards);
--- app/backend/src/routes/leaderboard.route.ts
+++ app/backend/src/routes/leaderboard.route.ts
@@ -4,6 +4,7 @@ import LeaderboardsController from '../controllers/leaderboard.controller';
 
 const leaderboardsRouter = Router();
 
+leaderboardsRouter.get('/home', LeaderboardsController.homeTeamsGetAll);
 leaderboardsRouter.get('/', LeaderboardsController.findAll);
 
 export default leaderboardsRouter;
