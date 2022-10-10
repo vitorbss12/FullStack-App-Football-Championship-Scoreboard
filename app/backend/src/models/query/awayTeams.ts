9dc96e0b4e10df634b4f69ae77b123f9d3332e3f
vitorbss12 <vitor_bss10@hotmail.com>
2022-10-09 21:37:37 -0300

    feat: Query para jogos de fora

---
+++ app/backend/src/models/query/awayTeams.ts
@@ -0,0 +1,53 @@
+const awayTeamsQuery = `
+  SELECT
+  main.name,
+  ((main.totalVictories * 3) + totalDraws) as totalPoints,
+    main.totalGames,
+    main.totalVictories,
+    main.totalDraws,
+    main.totalLosses,
+    main.goalsFavor,
+    main.goalsOwn,
+    (main.goalsFavor - main.goalsOwn) as goalsBalance
+  FROM 
+  (SELECT
+    t.team_name as name,
+    SUM(
+      CASE m.in_progress
+      WHEN false THEN 1 ELSE 0
+      END
+    ) as totalGames,
+    SUM(
+      CASE (m.away_team_goals > m.home_team_goals)
+      WHEN true THEN 1 ELSE 0
+      END
+    ) as totalVictories,
+    SUM(
+      CASE (m.home_team_goals = m.away_team_goals)
+      WHEN true THEN 1 ELSE 0
+      END
+    ) as totalDraws,
+    SUM(
+      CASE (m.away_team_goals < m.home_team_goals)
+      WHEN true THEN 1 ELSE 0
+      END
+    ) as totalLosses,
+    SUM(m.away_team_goals) as goalsFavor,
+    SUM(m.home_team_goals) as goalsOwn
+  FROM
+    TRYBE_FUTEBOL_CLUBE.matches as m
+  INNER JOIN
+    TRYBE_FUTEBOL_CLUBE.teams as t
+  ON
+    m.away_team = t.id AND m.in_progress = 0
+  GROUP BY
+    name
+  ) as main
+    ORDER BY
+      totalPoints DESC,
+      totalVictories DESC,
+      goalsBalance DESC,
+      goalsFavor DESC,
+      goalsOwn DESC;`;
+
+export default awayTeamsQuery;
