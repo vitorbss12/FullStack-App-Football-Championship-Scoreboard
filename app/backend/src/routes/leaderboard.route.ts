1f80b7e2faddf17a5dd6a2b0f203846d0b1f4660
vitorbss12 <vitor_bss10@hotmail.com>
2022-10-09 21:38:59 -0300

    feat: Implementando rota para leaderbords/away

--- app/backend/src/routes/leaderboard.route.ts
+++ app/backend/src/routes/leaderboard.route.ts
@@ -5,6 +5,7 @@ import LeaderboardsController from '../controllers/leaderboard.controller';
 const leaderboardsRouter = Router();
 
 leaderboardsRouter.get('/home', LeaderboardsController.homeTeamsGetAll);
+leaderboardsRouter.get('/away', LeaderboardsController.awayTeamsGetAll);
 leaderboardsRouter.get('/', LeaderboardsController.findAll);
 
 export default leaderboardsRouter;
