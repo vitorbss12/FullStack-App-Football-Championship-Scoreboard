6b48ed3cfa18a79ec31e6201e3e6df780a604cda
vitorbss12 <vitor_bss10@hotmail.com>
2022-10-10 11:59:12 -0300

    chore: Alterando nome da função chamada em service

--- app/backend/src/routes/leaderboard.route.ts
+++ app/backend/src/routes/leaderboard.route.ts
@@ -6,6 +6,6 @@ const leaderboardsRouter = Router();
 
 leaderboardsRouter.get('/home', LeaderboardsController.homeTeamsGetAll);
 leaderboardsRouter.get('/away', LeaderboardsController.awayTeamsGetAll);
-leaderboardsRouter.get('/', LeaderboardsController.findAll);
+leaderboardsRouter.get('/', LeaderboardsController.fullTeamsGetAll);
 
 export default leaderboardsRouter;
