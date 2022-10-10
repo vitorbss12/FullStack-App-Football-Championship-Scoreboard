ac10d10050847a71cce91dfefeac4e3b1698a42a
vitorbss12 <vitor_bss10@hotmail.com>
2022-10-09 21:38:39 -0300

    feat: Controller para Leaderboard Away

--- app/backend/src/controllers/leaderboard.controller.ts
+++ app/backend/src/controllers/leaderboard.controller.ts
@@ -7,6 +7,11 @@ export default class LeaderboardsController {
     return res.status(200).json(homeTeams);
   }
 
+  static async awayTeamsGetAll(_req: Request, res: Response) {
+    const awayTeams = await LeaderboardsService.awayTeamsGetAll();
+    return res.status(200).json(awayTeams);
+  }
+
   static async findAll(_req: Request, res: Response) {
     const leaderboards = await LeaderboardsService.getTeamsInfo();
     res.status(200).json(leaderboards);
